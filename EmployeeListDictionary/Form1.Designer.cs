4b3b1fe232ab5b83ca763f5a15ab5c95ca3e11df
Tom Johnson <59239002+Otterbob0109@users.noreply.github.com>
2021-09-21 11:37:30 -0500

    Add text and names to UI objects

--- EmployeeListDictionary/Form1.Designer.cs
+++ EmployeeListDictionary/Form1.Designer.cs
@@ -29,88 +29,88 @@ namespace EmployeeListDictionary
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstNames = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtEmpID = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstNames
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 25;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(252, 429);
-            this.listBox1.TabIndex = 0;
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.ItemHeight = 25;
+            this.lstNames.Location = new System.Drawing.Point(12, 12);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(252, 429);
+            this.lstNames.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(333, 46);
+            this.label1.Location = new System.Drawing.Point(270, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.Size = new System.Drawing.Size(142, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Employee Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(333, 93);
+            this.label2.Location = new System.Drawing.Point(270, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.Size = new System.Drawing.Size(113, 25);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Employee ID";
             // 
-            // textBox1
+            // txtEmpName
             // 
-            this.textBox1.Location = new System.Drawing.Point(418, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(234, 31);
-            this.textBox1.TabIndex = 3;
+            this.txtEmpName.Location = new System.Drawing.Point(418, 43);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(234, 31);
+            this.txtEmpName.TabIndex = 3;
             // 
-            // textBox2
+            // txtEmpID
             // 
-            this.textBox2.Location = new System.Drawing.Point(418, 90);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(234, 31);
-            this.textBox2.TabIndex = 4;
+            this.txtEmpID.Location = new System.Drawing.Point(418, 90);
+            this.txtEmpID.Name = "txtEmpID";
+            this.txtEmpID.Size = new System.Drawing.Size(234, 31);
+            this.txtEmpID.TabIndex = 4;
             // 
-            // button1
+            // btnAdd
             // 
-            this.button1.Location = new System.Drawing.Point(364, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(217, 34);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAdd.Location = new System.Drawing.Point(364, 188);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(217, 34);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add New Employee";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnSave
             // 
-            this.button2.Location = new System.Drawing.Point(364, 261);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(217, 34);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSave.Location = new System.Drawing.Point(364, 261);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(217, 34);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtEmpID);
+            this.Controls.Add(this.txtEmpName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstNames);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Employee List";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,13 +118,13 @@ namespace EmployeeListDictionary
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstNames;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtEmpID;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
